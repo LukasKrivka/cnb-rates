1ce8f4ccb691913f962d22a33552cf9c2f1c727f
LK <home@LKs-MacBook-Air.local>
2023-10-14 08:56:54 +0200

    fix typo in setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 python3 -m venv rates-venv
-source rates.venv/bin/activate
+source rates-venv/bin/activate
 pip3 install -r requirements.txt || pip3 install pandas==1.5.2 pytz==2022.7.1 requests==2.28.2
 python3 src/main.py
 deactivate
